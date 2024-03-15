fbff25b45200f0e2d2cc7d1c73364a4c91990068
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-15 14:39:58 -0700

    ok

--- main.hpp
+++ main.hpp
@@ -1,8 +1,61 @@
+#include <iostream>
+using namespace std;
+int getNextPrime(int);
+int getPrevPrime(int);
+void getTwoValues(int& begin, int& end)
+{
+    for (int i = 0; i < 1;)
+    {
+        cin>>begin>>end;
+        if ((begin<end))
+        {       
+            i++;
+        }
+    }
+    
+    
+}
 
-//********************
-// Declare Function Prototypes
-//********************
+int getNextPrime(int begin)
+{
+int k,h,flag=0;
+	for (h = begin; h < (begin+10);h++)
+	{
+        flag=0;
+		for (  k= 2; k <= (h/2); k++)
+		{
+			if (h%k==0)
+			{
+				flag=1; break;
+			}
+		}
+		if (flag==0)
+		{
+            return h;
+            h=begin+10;
+		}
+	}
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+int getPrevPrime(int end)
+{
+
+int k,h,flag=0;
+	for (h = end; h > (end-10);h--)
+	{
+        flag=0;
+		for (  k= 2; k <= (h/2); k++)
+		{
+			if (h%k==0)
+			{
+				flag=1; break;
+			}
+		}
+		if (flag==0)
+		{
+            return h;
+            h=end-10;
+		}
+	}
+}
+
